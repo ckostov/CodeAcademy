9b5abbaee563a83a0dd4c2836b903612acdb14e2
ckostov <cekokostov@yahoo.com>
2021-10-06 08:51:59 +0300

    Create zadacha17.c

---
+++ zadacha17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char number[10] = " ";
+    int counter = 0;
+    printf("Type a number: ");
+    scanf("%s", &number);
+    for(int i = 0; i <= sizeof(number); i++){
+        if(number[i] == '0' && number[i+1] == '1' && number[i+2] == '0'){
+            counter++;
+        }
+    }
+    printf("%d", counter);
+    return 0;
+}
